46c206f6f42932445a1a9c9cc4bda691760e6483
manuelzarroca <71357308+manuelzarroca@users.noreply.github.com>
2023-08-18 20:16:53 -0600

    Delete src directory

--- src/laugh-ca.js
+++
@@ -1,8 +0,0 @@
-var punsarray = Array(
-  "hola",
-  "adios",
-);
-var puns = function () {
-  var newpun = punsarray[Math.floor(Math.random() * punsarray.length)];
-  document.getElementById("laugh-text").innerHTML = newpun;
-};
